e8754d58da85c692d39c516c853df98720d78ad8
www95 <josephfernan_des@hotmail.com>
2020-12-15 18:48:35 -0200

    first commit 15.12.2020

---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+
+
+    private Transform backPoint;
+    
+    void Start()
+    {
+        backPoint = GameObject.Find("backPoint").GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        if(transform.position.x < backPoint.position.x){
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/PlatformGeneration.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformGeneration : MonoBehaviour
+{
+    public GameObject Platform;
+    public Transform point;
+    public float Distance;
+
+    private float platformWidth; 
+
+    void Start()
+    {
+        if(Platform.GetComponent<BoxCollider2D>()){
+            platformWidth = Platform.GetComponent<BoxCollider2D>().size.x;
+
+        }else{
+            platformWidth = Platform.GetComponent<PolygonCollider2D>().bounds.size.x;
+        }
+    }
+
+    void Update()
+    {
+        Distance = Random.Range(3f, 8f);
+
+        if(transform.position.x < point.position.x){
+            transform.position = new Vector3(transform.position.x + platformWidth + Distance, transform.position.y, transform.position.z);
+            Instantiate(Platform, transform.position, transform.rotation);
+        }   
+    }
+}
